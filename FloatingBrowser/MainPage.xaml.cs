4f47e43483441f1d203039d3717532e0d0632290
Dominik Tarnowski <dominik.t.uk@gmail.com>
2018-05-08 10:29:54 +0100

    feat: custom youtube url handling

--- FloatingBrowser/MainPage.xaml.cs
+++ FloatingBrowser/MainPage.xaml.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Windows.ApplicationModel.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System;
 using Windows.UI;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
@@ -51,9 +54,50 @@ namespace FloatingBrowser
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            webView.Source = new Uri(urlBox.Text);
-            webView.Visibility = Visibility.Visible;
-            initialPanel.Visibility = Visibility.Collapsed;
+            GoToUrl();
+        }
+
+        private void urlBox_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == VirtualKey.Enter)
+            {
+                GoToUrl();
+            }
+        }
+
+        public void GoToUrl()
+        {
+            string url = urlBox.Text;
+
+            string youtubeUrlPattern = @"((http(s?))\:\/\/)?(www\.)?(youtube\.com|youtu\.be)\/(embed\/|watch\?v=)?(?<id>[A-Za-z0-9-_]{11})";
+            var matches = Regex.Matches(urlBox.Text, youtubeUrlPattern, RegexOptions.IgnoreCase);
+            var regex = new Regex(youtubeUrlPattern);
+
+            if (Regex.IsMatch(urlBox.Text, youtubeUrlPattern, RegexOptions.IgnoreCase))
+            {
+                var groups = regex.Match(url).Groups;
+
+                foreach (string groupName in regex.GetGroupNames())
+                {
+                    if (groupName == "id")
+                    {
+                        url = "https://youtube.com/embed/"+groups[groupName].Value;
+                    }
+                }
+            }
+
+            Debug.WriteLine(url);
+            
+            try
+            {
+                Uri uri = new Uri(url);
+                webView.Source = uri;
+                webView.Visibility = Visibility.Visible;
+                initialPanel.Visibility = Visibility.Collapsed;
+            } catch(Exception)
+            {
+                urlBox.Text = "";
+            }
         }
     }
 }
