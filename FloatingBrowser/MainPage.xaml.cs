17aff3a8a09445f74c98cbc687e3c56c6eba2b17
Dominik Tarnowski <dominik.t.uk@gmail.com>
2018-05-08 09:15:46 +0100

    Initial commit

--- FloatingBrowser/MainPage.xaml.cs
+++ FloatingBrowser/MainPage.xaml.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +26,34 @@ namespace FloatingBrowser
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        
         public MainPage()
         {
             this.InitializeComponent();
+
+            ApplicationViewTitleBar formattableTitleBar = ApplicationView.GetForCurrentView().TitleBar;
+            formattableTitleBar.ButtonBackgroundColor = Colors.Transparent;
+            CoreApplicationViewTitleBar coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            coreTitleBar.ExtendViewIntoTitleBar = true;
+
+            EnterViewMode(ApplicationViewMode.CompactOverlay);
+        }
+
+        public async void EnterViewMode(ApplicationViewMode mode)
+        {
+            bool switched = false;
+            if (ApplicationView.GetForCurrentView().IsViewModeSupported(mode))
+            {
+                switched = await ApplicationView.GetForCurrentView().TryEnterViewModeAsync(mode);
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            webView.Source = new Uri(urlBox.Text);
+            webView.Visibility = Visibility.Visible;
+            initialPanel.Visibility = Visibility.Collapsed;
         }
     }
 }
